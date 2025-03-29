56e18032ba0f510816c9db47ab42b8216470af2b
Jonathan LAURENT <johjo@ytreza.org>
2025-03-29 16:53:58 +0100

    chore: change type of abstraction

--- src/todolist_hexagon/read_adapter_dependencies.py
+++ src/todolist_hexagon/read_adapter_dependencies.py
@@ -1,10 +1,15 @@
+from abc import ABC, abstractmethod
+
 from typing_extensions import Protocol
 
 from todolist_hexagon.fvp.aggregate import FvpSessionSetPort
 from todolist_hexagon.fvp.read.which_task import TodolistPort
 
 
-class ReadAdapterDependenciesPort(Protocol):
+class ReadAdapterDependenciesPort(ABC):
+
+    @abstractmethod
     def todolist(self) -> TodolistPort: ...
 
+    @abstractmethod
     def fvp_session_set(self) -> FvpSessionSetPort: ...
